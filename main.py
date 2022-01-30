e083d663fcdcfccb1510c4b2576a1dc1a7eed2de
YosufHassan <98308374+YosufHassan@users.noreply.github.com>
2022-01-30 11:16:28 +0800

    Fix bugs with the columnar transposition decryption algorithm
    Fix the bugs related to the columnar transposition cipher. In addition to changing the plaintext to include larger text body

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import string
 import itertools
 import math
 
-key = "multimedia"
+key = "qvgcwxiybjspzfadtelnkuorhm"
 ALPHABET_SIZE = 26
 
 # Global Variables and functions
@@ -303,7 +303,7 @@ class PlayFair():
     
     def encode(self,plaintext, key):
         table = self.generate_table(key)
-        print(f'The Key Matrix\n{table}')
+        print(f'Constructing square key matrix\n\n{table}\n')
         plaintext = self.prepare_input(plaintext)
         ciphertext = ""
 
@@ -326,7 +326,7 @@ class PlayFair():
     
     def decode(self,ciphertext, key):
         table = self.generate_table(key)
-        print(f'The Key Matrix\n{table}')
+        print(f'Constructing the square key matrix\n\n{table}\n')
         plaintext = ""
     
         for char1, char2 in self.chunker(ciphertext, 2):    # Each pair identify which row and column the pair exists (identify pair position)
@@ -349,90 +349,90 @@ class ColumnTransposition():
 # Encryption
     def encryptMessage(self,msg):
         cipher = ""
-
+    
         # track key indices
         k_indx = 0
-
+    
         msg_len = float(len(msg))
         msg_lst = list(msg)
         key_lst = sorted(list(key))
-
+    
         # calculate column of the matrix
         col = len(key)
         
         # calculate maximum row of the matrix
         row = int(math.ceil(msg_len / col))
-
+    
         # add the padding character '_' in empty
-        # the empty cell of the matix
+        # the empty cell of the matix 
         fill_null = int((row * col) - msg_len)
         msg_lst.extend('_' * fill_null)
-
-        # create Matrix and insert message and
-        # padding characters row-wise
-        matrix = [msg_lst[i: i + col]
+    
+        # create Matrix and insert message and 
+        # padding characters row-wise 
+        matrix = [msg_lst[i: i + col] 
                 for i in range(0, len(msg_lst), col)]
-
+    
         # read matrix column-wise using key
         for _ in range(col):
             curr_idx = key.index(key_lst[k_indx])
-            cipher += ''.join([row[curr_idx]
+            cipher += ''.join([row[curr_idx] 
                             for row in matrix])
             k_indx += 1
-
+    
         return cipher
-
+    
     # Decryption
     def decryptMessage(self,cipher):
         msg = ""
-
+    
         # track key indices
         k_indx = 0
-
+    
         # track msg indices
         msg_indx = 0
         msg_len = float(len(cipher))
         msg_lst = list(cipher)
-
+    
         # calculate column of the matrix
         col = len(key)
         
         # calculate maximum row of the matrix
         row = int(math.ceil(msg_len / col))
-
-        # convert key into list and sort
-        # alphabetically so we can access
+    
+        # convert key into list and sort 
+        # alphabetically so we can access 
         # each character by its alphabetical position.
         key_lst = sorted(list(key))
-
-        # create an empty matrix to
+    
+        # create an empty matrix to 
         # store deciphered message
         dec_cipher = []
         for _ in range(row):
             dec_cipher += [[None] * col]
-
-        # Arrange the matrix column wise according
+    
+        # Arrange the matrix column wise according 
         # to permutation order by adding into new matrix
         for _ in range(col):
             curr_idx = key.index(key_lst[k_indx])
-
+    
             for j in range(row):
                 dec_cipher[j][curr_idx] = msg_lst[msg_indx]
                 msg_indx += 1
             k_indx += 1
-
+    
         # convert decrypted msg matrix into a string
         try:
             msg = ''.join(sum(dec_cipher, []))
         except TypeError:
-            raise TypeError("This program cannot",
-                            "decrypt repeating words.")
-
+            raise TypeError("Key not compatible with the algorithm",
+                            "Change key to not include duplicate letters")
+    
         null_count = msg.count('_')
-
+    
         if null_count > 0:
             return msg[: -null_count]
-
+    
         return msg
 
 class AES():
@@ -608,7 +608,6 @@ print(columnres)
 print(f'\nTime taken to perform the Column Transposition encryption algrithm is {columnend-columnstart} \n')
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 print("\nPerforming the Column Transposition decryption algorithm ...\n")
-columnres = []
 columndecstart = time.time()
 columndecres = ColumnTransposition().decryptMessage(columnres)
 columndecend = time.time()
